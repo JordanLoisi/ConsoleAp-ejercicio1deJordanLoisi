6f12b3f60d6308e59ef3547d3122c4a0393ad5b1
silvi <silvi@LAPTOP-GES52B11>
2022-07-05 20:20:51 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAp ejercicio1deJordanLoisi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAp_ejercicio1deJordanLoisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double dolar;
+			double euro;
+			int pesos;
+
+			Console.Write("ingrese el valor de pesos");
+			pesos = int.Parse(Console.ReadLine());
+
+
+
+			dolar = pesos / 230;
+			euro = dolar / 1.17;
+
+			Console.WriteLine($"la cantidad de dolar que puedo comprar es  {dolar}");
+			Console.WriteLine($"la cantidad de euros que puedo comprar es  {euro}");
+		}
+    }
+}
